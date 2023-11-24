f8da6ab92ad5f83d047bc941d47175f0e5347f5c
Johan Hennius <johan.bh19@gmail.com>
2023-11-24 10:31:49 +0000

    CRUD functionality complete

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Recipe
@@ -40,3 +41,26 @@ class AddRecipe(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddRecipe, self).form_valid(form)
+
+
+class EditRecipe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    Update a recipe
+    """
+    template_name = 'recipes/edit_recipe.html'
+    model = Recipe
+    form_class = RecipeForm
+    success_url = '/recipes/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
+
+class DeleteRecipe(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """
+    Delete a recipe
+    """
+    model = Recipe
+    success_url = '/recipes/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
