0b063c71eda907113f36b61b772750dc2b145eaf
Johan Hennius <johan.bh19@gmail.com>
2023-11-24 12:09:32 +0000

    Final commit

--- recipe_share_app/views.py
+++ recipe_share_app/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from recipes.models import Recipe
 
-
-class Index(TemplateView):
+class Index(ListView):
     template_name = 'recipe_share_app/index.html'
+    model = Recipe
+    context_object_name = "recipes"
+
+    def get_queryset(self):
+        return self.model.objects.all()[:3]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 
+from django.db.models import Q
+
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -16,6 +18,20 @@ class Recipes(ListView):
     template_name = "recipes/recipes.html"
     model = Recipe
     context_object_name = "recipes"
+
+    def get_queryset(self, **kwargs):
+        query = self.request.GET.get("search")
+        if query:
+            recipes = self.model.objects.filter(
+                Q(title__icontains=query) | 
+                Q(description__icontains=query) |
+                Q(instructions__icontains=query) |
+                Q(cuisine_types__icontains=query)
+            )
+        else:
+            recipes = self.model.objects.all()
+        return recipes
+        
 
 
 class RecipeDetail(DetailView):
